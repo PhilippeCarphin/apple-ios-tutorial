3a5cf92ed340588e99cfc3febbebcaa2c0915aa1
Philippe Carphin <phil103@hotmail.com>
2024-12-29 01:46:38 -0500

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Philippe Carphin on 2024-12-29.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LandmarkTests/LandmarkTests.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkTests.swift
+//  LandmarkTests
+//
+//  Created by Philippe Carphin on 2024-12-29.
+//
+
+import Testing
+@testable import Landmark
+
+struct LandmarkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
