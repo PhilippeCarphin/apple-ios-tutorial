1d63a35e689813c3535e5e0979ad3b9ebecaeac3
Philippe Carphin <phil103@hotmail.com>
2024-12-29 12:47:43 -0500

    Tutorial first part

---
+++ Landmark/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Landmark
+//
+//  Created by Philippe Carphin on 2024-12-29.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
